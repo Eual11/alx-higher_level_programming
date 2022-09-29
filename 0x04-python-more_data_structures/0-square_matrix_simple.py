479dad118a4740110bea3e92ac9e4090cc0d2444
Eual11 <uchihaeual11@gmail.com>
2022-09-29 23:48:04 +0300

    uploading from local machine

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,20 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]) -> list:
     """squares matric"""
-
+    temp_list = matrix[:]
     outlist = []
 
-    for list in matrix:
+    for list in temp_list:
         temp = [value**2 for value in list]
         outlist.append(temp)
     return outlist
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
