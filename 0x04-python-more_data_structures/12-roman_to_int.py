5a84f2ef189ee64aa2b8fc88bce0e770225901c8
Eual11 <uchihaeual11@gmail.com>
2022-09-30 03:27:50 +0300

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import string
+
+
+def roman_to_int(roman_string):
+    if (not isinstance(roman_string, str)):
+        return None
+    va_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
+    int_value = 0
+    for char in roman_string:
+        int_value += va_dict[char]
+    return int(int_value)
