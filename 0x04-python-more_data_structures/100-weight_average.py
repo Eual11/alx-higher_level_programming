e684edcc51ede70b517e5a9e2fad566dcf240571
Eual11 <uchihaeual11@gmail.com>
2022-09-30 21:13:45 +0300

    fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -7,6 +7,8 @@ def weight_average(my_list=[]):
     sum = 0
     weight = 0
     for value in my_list:
+        if (len(tuple(value)) != 2):
+            return 0
         sum += (value[0]*value[1])
         weight += value[i]
     return sum/weight
