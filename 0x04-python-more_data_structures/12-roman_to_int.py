30f50a345b2421e1dba95ac0839fb590c3bfc25f
Eual11 <uchihaeual11@gmail.com>
2022-09-30 03:51:30 +0300

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import string
-
-
 def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or roman_string is None):
         return 0
     va_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
     int_value = 0
-    for char in roman_string:
-        if (not va_dict.get(char, 0)):
+    for i in range(0, len(roman_string)):
+        if (not va_dict.get(roman_string[i], 0)):
             return 0
-        int_value += va_dict[char]
+        if (i < len(roman_string) - 1 and
+           va_dict.get(roman_string[i]) < va_dict[roman_string[i+1]]):
+            int_value -= va_dict[roman_string[i]]
+        else:
+            int_value += va_dict[roman_string[i]]
     return int(int_value)
