b5ab4bf4eedfeaf726cea89d0a5777a7d2fdd612
Eual11 <uchihaeual11@gmail.com>
2022-09-30 20:59:10 +0300

    weight

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,12 +1,12 @@
-from itertools import count
+#!/usr/bin/python3
 
 
 def weight_average(my_list=[]):
-    if (not len(my_list)):
+    if (not my_list):
         return 0
     sum = 0
     weight = 0
     for value in my_list:
         sum += (value[0]*value[1])
-        weight += value[1]
+        weight += value[i]
     return sum/weight
