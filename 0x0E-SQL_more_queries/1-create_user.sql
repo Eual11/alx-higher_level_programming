9e951c6dec6c32571e8b49488001ecec5ae0d7e5
Eual11 <uchihaeual11@gmail.com>
2022-12-01 20:46:37 +0300

    more quries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creating user and granting all privileges
+
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1@localhost';
+FLUSH PRIVILEGES;
