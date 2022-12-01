8b25bb325802d08a4dfb10d546e18e1c28ab53a1
Eual11 <uchihaeual11@gmail.com>
2022-12-01 22:44:08 +0300

    q

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,4 @@
+-- unique id
+CREATE TABLE IF NOT EXISTS `unique_id`(
+	id INT UNIQUE DEFAULT 1,
+	name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- cities
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.cities`(
+	id INT AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
