2bd427b83f91e59da2d752339053ca1ae7214311
Eual11 <uchihaeual11@gmail.com>
2022-12-20 02:23:28 +0300

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const sl = process.argv.map(Number).slice(2, process.argv.length).sort().reverse()[1];
-  console.log(sl);
+  const args = process.argv.map(Number).slice(2, process.argv.length).sort((a, b) => a - b);
+  const sl = args[(args.length-2)];
+  console.log(sl) 
 }
