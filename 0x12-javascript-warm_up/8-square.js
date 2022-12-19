0f46fa6da1a344376467647699717c8604194012
Eual11 <uchihaeual11@gmail.com>
2022-12-20 01:31:39 +0300

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+const c = 'X';
+
+if (size) {
+  let i = 0;
+  while (i < size) {
+    console.log(c.repeat(size));
+    i++;
+  }
+} else {
+  console.log('Missing size');
+}
